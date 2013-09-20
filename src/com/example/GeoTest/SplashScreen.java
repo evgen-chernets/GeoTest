3611e9156afe9e477877be87d384e42b6c9acb82
evgen-chernets <evgen.chernets@mail.ru>
2013-09-20 16:46:19 +0300

    splash timeout increased layout modified

--- src/com/example/GeoTest/SplashScreen.java
+++ src/com/example/GeoTest/SplashScreen.java
@@ -28,7 +28,7 @@ public class SplashScreen extends Activity {
 
     private void prepare() {
         try {
-            Thread.sleep(500);
+            Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
