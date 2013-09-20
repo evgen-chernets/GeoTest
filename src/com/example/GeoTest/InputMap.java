3b512f34e50edd4840cf7e47c33eba2748774a62
e.chernets <e.chernets@samsung.com>
2013-09-20 15:57:40 +0300

    fragment class added

---
+++ src/com/example/GeoTest/InputMap.java
@@ -0,0 +1,12 @@
+package com.example.GeoTest;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: e.chernets
+ * Date: 20.09.13
+ * Time: 11:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class InputMap extends MapFragment {
+}
