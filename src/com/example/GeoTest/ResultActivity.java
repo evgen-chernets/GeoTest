edfb2dbb7a78eab5285e086026e229282b934f13
e.chernets <e.chernets@samsung.com>
2013-09-30 15:54:07 +0300

    result activity updated some bugs fixed

--- src/com/example/GeoTest/ResultActivity.java
+++ src/com/example/GeoTest/ResultActivity.java
@@ -6,18 +6,20 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -44,47 +46,34 @@ public class ResultActivity extends FragmentActivity implements GooglePlayServic
     private GoogleMap mapResult;
     private LocationClient locationClient;
     private LocationRequest locationRequest;
+    private String urlString;
+    private String routeSearchResult;
+    RequestRoute requestRoute;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
-
-        if (mapResult == null) {
-            mapResult = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_result)).getMap();
-            if (mapResult != null) {
-                mapResult.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            }
-        }
-        if (locationClient == null) {
-            locationClient = new LocationClient(this, this, this);
-        }
-        if (locationRequest == null) {
-            locationRequest = LocationRequest.create();
-            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            locationRequest.setInterval(3000);
-            locationRequest.setFastestInterval(1000);
-        }
-
-        Intent intent = getIntent();
-        Double latFrom = intent.getDoubleExtra("from_lat", 0);
-        Double lonFrom = intent.getDoubleExtra("from_lon", 0);
-        Double latTo = intent.getDoubleExtra("to_lat", 0);
-        Double lonTo = intent.getDoubleExtra("to_lon", 0);
-        pointFrom = new LatLng(latFrom, lonFrom);
-        pointTo = new LatLng(latTo, lonTo);
-        String link = "http://maps.googleapis.com/maps/api/directions/json?";
-        String params = String.format("origin=%f,%f&destination=%f,%f&sensor=false", latFrom, lonFrom, latTo, lonTo);
-        String urlString = link + params;
-
-        RequestRoute requestRoute = new RequestRoute();
-        requestRoute.execute(urlString);
-
+        initMapsAndLocations();
+        getDataFromIntent();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         locationClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestRoute = new RequestRoute();
+        requestRoute.execute(urlString);
+    }
+
+    @Override
+    protected void onPause() {
+        requestRoute.cancel(true);
+        super.onPause();
     }
 
     @Override
@@ -123,56 +112,123 @@ public class ResultActivity extends FragmentActivity implements GooglePlayServic
         }
     }
 
+    private void initMapsAndLocations() {
+        if (mapResult == null) {
+            mapResult = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_result)).getMap();
+            if (mapResult != null) {
+                mapResult.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        }
+        if (locationClient == null) {
+            locationClient = new LocationClient(this, this, this);
+        }
+        if (locationRequest == null) {
+            locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(3000);
+            locationRequest.setFastestInterval(1000);
+        }
+    }
+
+    private void getDataFromIntent() {
+        Intent intent = getIntent();
+        Double latFrom = intent.getDoubleExtra("from_lat", 0);
+        Double lonFrom = intent.getDoubleExtra("from_lon", 0);
+        Double latTo = intent.getDoubleExtra("to_lat", 0);
+        Double lonTo = intent.getDoubleExtra("to_lon", 0);
+        pointFrom = new LatLng(latFrom, lonFrom);
+        pointTo = new LatLng(latTo, lonTo);
+        String link = "http://maps.googleapis.com/maps/api/directions/json?";
+        String params = String.format("origin=%f,%f&destination=%f,%f&sensor=false", latFrom, lonFrom, latTo, lonTo);
+        urlString = link + params;
+    }
+
     private class RequestRoute extends AsyncTask<String, Void, PolylineOptions> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            ((ProgressBar)findViewById(R.id.result_progressBar)).setVisibility(View.VISIBLE);
+            findViewById(R.id.result_progressBar).setVisibility(View.VISIBLE);
         }
 
         @Override
         protected PolylineOptions doInBackground(String... params) {
             PolylineOptions polyOptions = new PolylineOptions();
-            try {
-                URL url = new URL(params[0]);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                byte[] response = new byte[inputStream.available()];
-                inputStream.read(response);
-                JSONObject jsonObject = new JSONObject(new String(response));
-                JSONArray routes = jsonObject.getJSONArray("routes");
-                JSONArray legs = ((JSONObject)routes.get(0)).getJSONArray("legs");
-                JSONArray steps = ((JSONObject)legs.get(0)).getJSONArray("steps");
-                String encoded_poly = null;
-                for (int i = 0; i < steps.length(); i++) {
-                    encoded_poly = steps.getJSONObject(i).getJSONObject("polyline").getString("points");
-                    polyOptions.addAll(decodePoints(encoded_poly));
+            Boolean responseReceived = false;
+            while (!responseReceived && !isCancelled()) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(requestDirections(params[0]));
+                    responseReceived = true;
+                    Log.d("GeoTest", "JSON object successfully constructed");
+                    routeSearchResult = jsonResponse.getString("status");
+                    if ("OK".equals(routeSearchResult)) {
+                        JSONArray routes = jsonResponse.getJSONArray("routes");
+                        JSONArray legs = ((JSONObject)routes.get(0)).getJSONArray("legs");
+                        JSONArray steps = ((JSONObject)legs.get(0)).getJSONArray("steps");
+                        String encoded_poly = null;
+                        for (int i = 0; i < steps.length(); i++) {
+                            encoded_poly = steps.getJSONObject(i).getJSONObject("polyline").getString("points");
+                            polyOptions.addAll(decodePoints(encoded_poly));
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d("GeoTest", "JSON exception");
+                    polyOptions = null;
                 }
-                httpURLConnection.disconnect();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                polyOptions = null;
-            } catch (IOException e) {
-                e.printStackTrace();
-                polyOptions = null;
-            } catch (JSONException e) {
-                e.printStackTrace();
-                polyOptions = null;
             }
+            if (polyOptions != null) Log.d("GeoTest", "Points decoded: " + polyOptions.getPoints().size());
             return polyOptions;
         }
 
         @Override
         protected void onPostExecute(PolylineOptions polyOptions) {
             super.onPostExecute(polyOptions);
+            TextView textView = (TextView)findViewById(R.id.result_text);
+            textView.setVisibility(View.VISIBLE);
+            textView.setText(routeSearchResult);
             if (polyOptions != null) {
-                LatLng center = calculateCenterPoint(pointFrom, pointTo, currentLocation);
-                mapResult.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 8));
-                polyOptions.width(20);
+                addMarker(pointFrom, "From");
+                addMarker(pointTo, "To");
+                addMarker(currentLocation, "Current");
+
+                polyOptions.width(10);
                 polyOptions.color(Color.BLACK);
                 mapResult.addPolyline(polyOptions);
-                ((ProgressBar)findViewById(R.id.result_progressBar)).setVisibility(View.INVISIBLE);
+
+                zoomAndCenter(pointFrom, pointTo, currentLocation);
+
+                findViewById(R.id.result_progressBar).setVisibility(View.INVISIBLE);
             }
+        }
+
+        private void  addMarker(LatLng target, String title) {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.draggable(false);
+            markerOptions.position(target);
+            markerOptions.title(title);
+            Marker marker = mapResult.addMarker(markerOptions);
+        }
+
+        private String requestDirections(String urlStr){
+            String result = null;
+            try {
+                URL url = new URL(urlStr);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                Thread.sleep(2000);
+                byte[] responseBytes = new byte[inputStream.available()];
+                inputStream.read(responseBytes);
+                httpURLConnection.disconnect();
+                Log.d("GeoTest", "Bytes from stream taken: " + responseBytes.length);
+                result = new String(responseBytes);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return result;
         }
 
         private List<LatLng> decodePoints(String encoded_points){
@@ -206,16 +262,14 @@ public class ResultActivity extends FragmentActivity implements GooglePlayServic
             return poly;
         }
 
-        private LatLng calculateCenterPoint(LatLng p1, LatLng p2, LatLng p3) {
-            double x1 = p1.latitude;
-            double y1 = p1.longitude;
-            double x2 = p2.latitude;
-            double y2 = p2.longitude;
-            double x3 = p3.latitude;
-            double y3 = p3.longitude;
-            double x = (Math.max(x1, Math.max(x2,x3))+ Math.min(x1, Math.min(x2,x3))) / 2;
-            double y = (Math.max(y1, Math.max(y2,y3))+ Math.min(y1, Math.min(y2,y3))) / 2;
-            return new LatLng(x,y);
+        private void zoomAndCenter(LatLng p1, LatLng p2, LatLng p3) {
+            LatLngBounds.Builder b = new LatLngBounds.Builder();
+            b.include(p1);
+            b.include(p2);
+            b.include(p3);
+            LatLngBounds bounds = b.build();
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 480, 480, 5);
+            mapResult.animateCamera(cu);
         }
     }
 }
