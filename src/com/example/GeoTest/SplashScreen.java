b46fc4f5734ad3056ceb3a751abea806d2624f24
Evgeniy Chernets <evgeniy.chernets@playtech.com>
2014-12-17 00:45:58 +0200

    minor changes

--- src/com/example/GeoTest/SplashScreen.java
+++ src/com/example/GeoTest/SplashScreen.java
@@ -1,10 +1,7 @@
 package com.example.GeoTest;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
